c604e989378c9eb50d3bef9de1c368722717048e
Daniel Leota <46200971+DrNykterstein@users.noreply.github.com>
2025-02-27 22:07:30 -0400

    update

--- Test.sql
+++ Test.sql
@@ -20,3 +20,9 @@ INNER JOIN pedido pe on dp.id_pedido = pe.id_pedido
 where (pe.fecha_pedido >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH))
 group by p.nombre_producto
 order by cantidad_vendida desc
+
+/* Identifica a los clientes que no han realizado ningun pedido en los ultimos 12 meses. Muestra su nombre, email y fecha de registro.*/
+SELECT c.nombre, c.email, c.fecha_registro
+FROM clientes c
+LEFT JOIN pedido p ON c.id_clientes = p.id_clientes AND p.fecha_pedido >= DATE_SUB(NOW(), INTERVAL 12 MONTH)
+WHERE p.id_pedido IS NULL
